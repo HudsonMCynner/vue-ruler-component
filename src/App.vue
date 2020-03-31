2e30b8906e7f9dd4534081ad37fb95458bfd186b
Hudson <hudsonmachado2012@gmail.com>
2020-03-31 09:19:31 -0400

    Upgrade to webpack project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
-    <ruler />
+    <img src="./assets/logo.png">
+    <ruler/>
   </div>
 </template>
 
 <script>
-import Ruler from './components/Ruler.vue'
+import Ruler from './components/Ruler'
 
 export default {
   name: 'App',
@@ -17,12 +18,11 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  background: #42b983;
 }
 </style>
